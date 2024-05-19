83f60b1ca84986e200f2c0230e80d9cbcf20ad13
Yao Chung Hu <30311066+FlashyReese@users.noreply.github.com>
2024-05-19 21:50:44 +0800

    change: Rename priority, blend alpha and use min/max alpha on fade

--- main.py
+++ main.py
@@ -20,6 +20,7 @@ manual_fix = []
 # Find all JSON files under exampledirectory/assets/*/sky/
 json_files = glob.glob(os.path.join(directory, "assets", "*", "sky", "**", "*.json"), recursive=True)
 
+print("Found", len(json_files), "JSON files under", directory)
 for file in json_files:
     with open(file, 'r+') as f:
         try:
@@ -31,7 +32,7 @@ for file in json_files:
         # Check if the file contains a schemaVersion key
         if 'schemaVersion' in json_data:
             # Check and remove conditions.loop key if present
-            if 'conditions' in json_data and 'loop' in json_data['conditions']:
+            if 'conditions' in json_data and 'loop' in json_data['conditions']: # todo: fix this and account for in fade
                 del json_data['conditions']['loop']
                 print("Removed conditions.loop key from", file)
 
@@ -65,6 +66,17 @@ for file in json_files:
                 elif current_type not in ["monocolor", "overworld", "end"]:
                     manual_fix.append(file)
 
+                # Rename properties priority if present
+                if 'properties' in json_data and 'priority' in json_data['properties']:
+                    json_data['properties']['layer'] = json_data['properties']['priority']
+                    del json_data['properties']['priority']
+                    print("Rename priority to layer", file)
+
+                # Rename blend mode alpha to normal if present
+                if 'blend' in json_data and 'type' in json_data['blend'] and json_data['blend']['type'] == "alpha":
+                    json_data['blend']['type'] = "normal"
+                    print("Updated blend mode in", file, "from alpha to normal")
+
                 # Migrate properties.fade object if present
                 if 'properties' in json_data and 'fade' in json_data['properties'] \
                         and all(key in json_data['properties']['fade'] for key in ['startFadeIn', 'endFadeIn', 'startFadeOut', 'endFadeOut']):
@@ -73,16 +85,25 @@ for file in json_files:
                     endFadeIn = fade_data['endFadeIn']
                     startFadeOut = fade_data['startFadeOut']
                     endFadeOut = fade_data['endFadeOut']
+
+                    # Check for maxAlpha and minAlpha in properties
+                    maxAlpha = json_data['properties'].get('maxAlpha', 1.0)
+                    minAlpha = json_data['properties'].get('minAlpha', 0.0)
+
                     json_data['properties']['fade']['keyFrames'] = {
-                        startFadeIn: 0.0,
-                        endFadeIn: 1.0,
-                        startFadeOut: 1.0,
-                        endFadeOut: 0.0
+                        startFadeIn: minAlpha,
+                        endFadeIn: maxAlpha,
+                        startFadeOut: maxAlpha,
+                        endFadeOut: minAlpha
                     }
+
                     del json_data['properties']['fade']['startFadeIn']
                     del json_data['properties']['fade']['endFadeIn']
                     del json_data['properties']['fade']['startFadeOut']
                     del json_data['properties']['fade']['endFadeOut']
+                    # Delete maxAlpha and minAlpha if they exist
+                    json_data['properties'].pop('maxAlpha', None)
+                    json_data['properties'].pop('minAlpha', None)
                     print("Migrated properties.fade in", file)
 
                 # Save the modified JSON data back to the file
